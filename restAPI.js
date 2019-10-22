f0105c7218574324248fb6f7e37a6224027d8e1f
CarlosEscat <carlosescat@gmail.com>
2019-10-22 17:46:28 +0200

    Connecting to database and showing all the messages with HTTP request

--- restAPI.js
+++ restAPI.js
@@ -1,17 +1,8 @@
 const express = require("express");
 const app = express();
-//const mongoose = require("mongoose");
 
 const router = require("./router");
 
-// mongoose.connect("mongodb://localhost:27017/myproject", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-// const db = mongoose.connection;
-// db.on("error", error => console.error(error));
-// db.once("open", () => console.log("connected to database"));
-// app.use(express.json());
 app.use(router);
 
 const port = process.env.PORT || 3000;
